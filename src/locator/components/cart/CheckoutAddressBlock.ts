ea1e9f7096774889da78564851eacbde0950db23
Huynh Minh Nghia <huynhminhnghia.hmn99@gmail.com>
2026-06-14 20:48:09 +0700

    feat: add CheckoutAddressBlock component

---
+++ src/locator/components/cart/CheckoutAddressBlock.ts
@@ -0,0 +1,16 @@
+import { Page } from '@playwright/test';
+import { BaseComponent } from '../baseComponent';
+
+export class CheckoutAddressBlock extends BaseComponent {
+  constructor(page: Page) {
+    super(page, '#address_delivery');
+  }
+
+  async getDeliveryAddress(): Promise<string> {
+    return (await this.page.locator('#address_delivery').textContent()) || '';
+  }
+
+  async getBillingAddress(): Promise<string> {
+    return (await this.page.locator('#address_invoice').textContent()) || '';
+  }
+}
